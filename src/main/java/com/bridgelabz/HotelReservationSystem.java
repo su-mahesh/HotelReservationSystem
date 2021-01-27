f963561cf22c9270a414b0de054571d63ab1650d
su-mahesh <maheshkangude@gmail.com>
2021-01-27 13:16:28 +0530

    REFACTORED: addHotel method

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,12 +1,22 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class HotelReservationSystem {
     private String hotelName;
     private Float rateForRegularCustomer;
 
+    HashMap<String, Float> rates;
+    HashMap<String,HashMap> hotels;
+
     public void addHotel(String hotelName, Float rateForRegularCustomer ) {
-        this.hotelName = hotelName;
-        this.rateForRegularCustomer = rateForRegularCustomer;
+
+        rates = new HashMap<>();
+        hotels = new HashMap<>();
+
+        rates.put("Rate for RegularCustomer", rateForRegularCustomer);
+        hotels.put(hotelName, rates);
     }
 
 
